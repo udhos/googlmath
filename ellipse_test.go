8e80811eb938e7818fee0f7b70e60aa3a9a597bb
Marcel Hauf <marcel.hauf@googlemail.com>
2014-05-06 22:25:54 +0200

    Improved ellipse tests

--- ellipse_test.go
+++ ellipse_test.go
@@ -4,16 +4,49 @@ import (
 	. "gopkg.in/check.v1"
 )
 
-func (s *S) TestEllipse(c *C) {
-	e := Ell(0, 0, 1, 1)
-	containTestTable := []TestValue_2F32_B{
-		TestValue_2F32_B{0, 0, true},
-		TestValue_2F32_B{1, 0, true},
-		TestValue_2F32_B{-2, -2, false},
-		TestValue_2F32_B{2, 0, false},
+type TestValue_4F32_Ell struct {
+	f0 float32
+	f1 float32
+	f2 float32
+	f3 float32
+	e  Ellipse
+}
+
+func (s *S) TestEllipseEll(c *C) {
+	tests := []TestValue_4F32_Ell{
+		{
+			0, 0, 0, 0, Ellipse{0, 0, 0, 0},
+		},
+		{
+			-1.2, -3.3, 1, 2, Ellipse{-1.2, -3.3, 1, 2},
+		},
+	}
+	for _, t := range tests {
+		obtained := Ell(t.f0, t.f1, t.f2, t.f3)
+		c.Assert(obtained, DeepEquals, t.e)
+	}
+}
+
+type TestValue_Ell2F32_B struct {
+	ell Ellipse
+	f0  float32
+	f1  float32
+	e   bool
+}
+
+func (s *S) TestEllipseContains(c *C) {
+	tests := []TestValue_Ell2F32_B{
+		{Ellipse{0, 0, 1, 1}, 0, 0, true},
+		{Ellipse{0, 0, 1, 1}, 1, 0, true},
+		{Ellipse{0, 0, 1, 1}, -2, -2, false},
+		{Ellipse{0, 0, 1, 1}, 2, 0, false},
+		{Ellipse{0, 0, 0, 1}, 2, 0, false},
+		{Ellipse{0, 0, 1, 0}, 2, 0, false},
+		{Ellipse{1, 2, 0, 0}, 2, 0, false},
 	}
 	// Contains
-	for i := range containTestTable {
-		c.Assert(e.Contains(containTestTable[i].X, containTestTable[i].Y), Equals, containTestTable[i].Expected)
+	for _, t := range tests {
+		obtained := t.ell.Contains(t.f0, t.f1)
+		c.Assert(obtained, Equals, t.e)
 	}
 }
