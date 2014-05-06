48edeef35fe65fd119a1ae26adecf50668fad32a
Marcel Hauf <marcel.hauf@googlemail.com>
2014-05-06 18:02:29 +0200

    Bezier2 tests

--- bezier2_test.go
+++ bezier2_test.go
@@ -21,20 +21,38 @@ func (s *S) TestBezier2Bzr2(c *C) {
 	}
 }
 
-type TestValue_PthF32_V2 struct {
+type TestValue_Pth2F32_V2 struct {
 	p Path2
 	f float32
 	e Vector2
 }
 
 func (s *S) TestBezier2ValueAt(c *C) {
-	tests := []TestValue_PthF32_V2{
-		{Bezier2(nil), 0.5, Vec2(0, 0)},
+	tests := []TestValue_Pth2F32_V2{
+		{Bezier2(nil), 0.5, Vec2(NaN(), NaN())},
 		{Bezier2{Vec2(0, 0), Vec2(1, 1)}, 0.5, Vec2(0.5, 0.5)},
 		{Bezier2{Vec2(0, 0), Vec2(-1, -1), Vec2(2, 2)}, 0.5, Vec2(0, 0)},
 	}
 	for _, t := range tests {
 		obtained := t.p.ValueAt(t.f)
-		c.Check(obtained, DeepEquals, t.e)
+		c.Check(obtained, Vector2Check, t.e)
+	}
+}
+
+type TestValue_Pth2V2_F32 struct {
+	p Path2
+	v Vector2
+	e float32
+}
+
+func (s *S) TestBezier2Approximate(c *C) {
+	tests := []TestValue_Pth2V2_F32{
+		{Bezier2(nil), Vec2(0, 0), NaN()},
+		{Bezier2{Vec2(0, 0), Vec2(1, 1)}, Vec2(0.5, 0.5), 0.5},
+		{Bezier2{Vec2(0, 0), Vec2(-1, -1), Vec2(2, 2)}, Vec2(0, 0), 0},
+	}
+	for _, t := range tests {
+		obtained := t.p.Approximate(t.v)
+		c.Check(obtained, EqualsFloat32, t.e)
 	}
 }
