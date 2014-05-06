81159a3b07f235bfed5063e36a16132d9071ab42
Marcel Hauf <marcel.hauf@googlemail.com>
2014-05-06 22:49:10 +0200

    Add empty triangle test value for IsPointInTriangle test.

--- intersector_test.go
+++ intersector_test.go
@@ -12,8 +12,9 @@ type IsPointInTriangleTestValue struct {
 
 func (s *S) TestIsPointInTriangle(c *C) {
 	containTestTable := []IsPointInTriangleTestValue{
-		IsPointInTriangleTestValue{Vec3(0.5, 0.5, 0), Vec3(0, 0, 0), Vec3(1, 1, 0), Vec3(0, 1, 0), true},
-		IsPointInTriangleTestValue{Vec3(2, 0.5, 0), Vec3(0, 0, 0), Vec3(1, 1, 0), Vec3(0, 1, 0), false},
+		{Vec3(0.5, 0.5, 0), Vec3(0, 0, 0), Vec3(1, 1, 0), Vec3(0, 1, 0), true},
+		{Vec3(2, 0.5, 0), Vec3(0, 0, 0), Vec3(1, 1, 0), Vec3(0, 1, 0), false},
+		{Vec3(1, 1, 1), Vec3(0, 0, 0), Vec3(0, 0, 0), Vec3(0, 0, 0), false},
 	}
 	for _, value := range containTestTable {
 		c.Assert(IsPointInTriangle(value.Point, value.T1, value.T2, value.T3), Equals, value.Expected)
